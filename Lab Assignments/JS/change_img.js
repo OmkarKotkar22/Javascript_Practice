3aa46a83ca822c0fc6ef43ebf3de0d13eb56548d
omkar <kotkaromkar048@gmail.com>
2026-03-26 09:45:20 +0530

    perform change img by using js

---
+++ Lab Assignments/JS/change_img.js
@@ -0,0 +1,7 @@
+function show() {
+    let imgInJs = document.getElementById('dry');
+    imgInJs.src="../assets/images2.webp";
+    imgInJs.style.width="60%";
+    imgInJs.style.height="400px";
+
+}
