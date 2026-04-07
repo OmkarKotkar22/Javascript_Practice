4cd45ebacdc111ff3a172a947c5a14bbfef9811a
omkar <kotkaromkar048@gmail.com>
2026-04-07 12:23:01 +0530

    perform check number is positive negative or zero

---
+++ Lab Assignments/JS/No_Even_Odd.js
@@ -0,0 +1,15 @@
+/*2. Even or Odd
+Logic:
+Check if number % 2 == 0 → even, else odd
+Example:
+Input: 8
+Output: Even
+*/
+
+let a = prompt("Enter the Number: ");
+if(a % 2 == 0){
+	console.log(a + " is Even");
+}
+else{
+	console.log(a + " is Odd");
+}
