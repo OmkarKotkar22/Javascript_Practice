86646d734eadbf1720f062f1216f93be2028b32b
omkar <kotkaromkar048@gmail.com>
2026-04-07 18:28:51 +0530

    find profit and loss

---
+++ Lab Assignments/JS/ProfitorLoss.js
@@ -0,0 +1,13 @@
+/*10. Profit or Loss
+Logic:
+Compare selling price (SP) and cost price (CP).
+Example:
+Input: CP=100, SP=120
+Output: Profit = 20
+*/
+
+let cp = 100;
+let sp = 200;
+
+let p = sp - cp;
+console.log("Profit = "+ p);
