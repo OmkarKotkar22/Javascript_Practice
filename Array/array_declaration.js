163905abd7e460c810a80fe195e6d0ebfdc864f5
Omkar Kotkar <143184805+OmkarKotkar22@users.noreply.github.com>
2026-04-11 21:59:38 +0530

    Add comments to array declaration and loop
    Added comments to clarify array declaration and iteration.

--- Array/array_declaration.js
+++ Array/array_declaration.js
@@ -1,5 +1,5 @@
-let a = new Array(10,20,30,40,50);
-let len = a.length;
-for(let i = 0; i < len; i++){
-    console.log(a[i]);
+let a = new Array(10,20,30,40,50); //declaration of array
+let len = a.length; //len declaration 
+for(let i = 0; i < len; i++){ //iterating the loop
+    console.log(a[i]);//print the elements 
 }
